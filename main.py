a0d7786214ab3b27c536e10d66fba0da3b168098
ib4519894 <ib4519894@lpssonline.com>
2021-08-23 14:59:16 +0000

    Complete Getting a Connection

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
+with engine.connect() as conn:
+
+    result = conn.execute(text("select 'hello world'"))
+    print(result.all())
+
+    
