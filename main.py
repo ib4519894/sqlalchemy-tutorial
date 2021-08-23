048047d18c4add29c063872aa44fdd62a36294da
ib4519894 <ib4519894@lpssonline.com>
2021-08-23 15:03:22 +0000

    Complete Committing Changes

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from sqlalchemy import create_engine, text
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
 with engine.connect() as conn:
 
-    result = conn.execute(text("select 'hello world'"))
-    print(result.all())
+    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 1, "y": 1}, {"x": 2, "y": 4}]
+    )
+    conn.commit()
 
-    
